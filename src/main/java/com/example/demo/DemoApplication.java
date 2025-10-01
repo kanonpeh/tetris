aa8c4c122a435f0492111df12cf7ce69f8b94454
kanon <kanonpeh@gmail,com>
2025-10-02 01:12:37 +0900

    replace console output with SpringApplication.run for application startup

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
-        System.out.println("javaの世界へ、ようこそ！");
+        SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
